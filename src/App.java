3e1ebfee3652bc93d66285b14e860e1b2d8cfdf3
liperm <feliperibeiromachado@edu.univali.br>
2023-10-17 21:48:28 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import gals.LexicalError;
+import gals.Lexico;
+import gals.SemanticError;
+import gals.Semantico;
+import gals.Sintatico;
+import gals.SyntaticError;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String texto = "a = 1 + 1;";
+        Lexico lex = new Lexico(texto);
+        Sintatico sin = new Sintatico();
+        Semantico sem = new Semantico();
+        try {
+            sin.parse(lex, sem);
+            System.out.println("deu bom");
+        } catch (LexicalError | SemanticError |SyntaticError ex) {
+            System.out.println("deu ruim");
+            
+        } 
+    }
+}
---
+++ src/gals/Semantico.java
@@ -0,0 +1,12 @@
+package gals;
+
+public class Semantico implements Constants {
+    public void executeAction(int action, Token token) throws SemanticError {
+        System.out.println("Ação #" + action + ", Token: " + token);
+        switch (action) {
+            case 1: {
+                
+            }
+        }
+    }
+}
